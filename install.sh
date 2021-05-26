289422e8f16203d6d404298c94cdc84ddd3ab0da
fawwazmayda <maydafawwaz@gmail.com>
2021-05-26 22:28:18 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+# Install GIT
+sudo apt-get install git -y
+
+# Install pip
+
+sudo apt-get install python3-pip -y
+
+# Upgrade pip
+
+pip3 install --upgrade pip
+
+# Require
+
+pip3 install -r requirements.txt
