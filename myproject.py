1fb66c4e793b9f9c804c4314b810ce104f3b485a
fawwazmayda <maydafawwaz@gmail.com>
2021-05-29 09:45:58 +0800

    add logging and flask cors

--- myproject.py
+++ myproject.py
@@ -2,9 +2,23 @@ from flask.helpers import send_file
 from MLManager import MLManager
 from flask import Flask,request,send_from_directory,jsonify
 import os
-import shutil
+import logging
+
+# Imports the Cloud Logging client library
+import google.cloud.logging
+
+# Instantiates a client
+client = google.cloud.logging.Client()
+
+# Retrieves a Cloud Logging handler based on the environment
+# you're running in and integrates the handler with the
+# Python logging module. By default this captures all logs
+# at INFO level and higher
+client.get_default_handler()
+client.setup_logging()
 
 app = Flask(__name__)
+app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.jpeg']
 model = MLManager("best.pt")
 
 # save the label and image path result
@@ -23,16 +37,31 @@ def detect():
     global model,image_result_path
     if request.method == "GET":
         return "To Detect from image use POST with 'file' with image to predict"
+
+    # Handle POST
+
+    # Validate Image is present and in correct file extension
+
+    print("Getting image from POST")
+    uploaded_image = request.files["file"]
+    if uploaded_image.filename == "":
+        # No file is present
+        msg = "No file is present"
+        logging.info(msg)
+        return msg
     
+    file_ext = os.path.splitext(uploaded_image.filename)[1]
+    if file_ext not in app.config['UPLOAD_EXTENSIONS']:
+        msg = "Image format not supported (only .jpeg, .jpg, .png)"
+        logging.info(msg)
+        return msg
+    
+
     # Delete previous image result
     model.delete_detection_folder()
     image_result_path = None
 
-    # Handle POST
-    print("Getting image from POST")
-    uploaded_image = request.files["file"]
-
-    # save to directory of images to infer
+    # save to directory of images-to-infer
     dir = os.path.join("images-to-infer",uploaded_image.filename)
     print(f"Save image to {dir}")
     uploaded_image.save(dir)
@@ -41,6 +70,7 @@ def detect():
     res = model.predict_image(dir)
     image_result_path = os.path.join("detection-result","exp",uploaded_image.filename)
     delete_uploaded_image(dir)
+    logging.debug(res)
     return jsonify(res)
 
 @app.route("/getImageDetectionResult",methods=["GET"])
